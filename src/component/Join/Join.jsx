2c4da657c45b0c3e02770025f093bb566a4f256f
Ahmet Karadaş <ahmetkaradas.official@gmail.com>
2023-06-19 23:42:08 +0300

    FitClub-Project

---
+++ src/component/Join/Join.jsx
@@ -0,0 +1,43 @@
+import React, {useRef} from 'react'
+import './Join.css'
+import emailjs from '@emailjs/browser'
+
+const Join = () => {
+    const form = useRef()
+
+    const sendEmail = (e) => {
+        e.preventDefault();
+    
+        emailjs.sendForm('service_z7wcmeg', 'template_5rkmcwg', form.current, 'q-mZm5aYgw18JPRtH')
+          .then((result) => {
+              console.log(result.text);
+          }, (error) => {
+              console.log(error.text);
+          });
+      };
+
+  return (
+    <div className="Join" id="join-us">
+      <div className="left-j">
+        <hr />
+        <div>
+          <span className="stroke-text">READY TO</span>
+          <span>LEVEL UP</span>
+        </div>
+        <div>
+          <span>YOUR BODY</span>
+          <span className="stroke-text">WITH US?</span>
+        </div>
+      </div>
+      <div className="right-j">
+        <form ref={form} className="email-container" onSubmit={sendEmail}>
+            <input type="email" name="user_email" placeholder="Please Enter Your Email" />
+            <button className="btn btn-j">Join Now</button>
+        </form>
+
+      </div>
+    </div>
+  );
+}
+
+export default Join
